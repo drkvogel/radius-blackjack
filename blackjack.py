c7a99f4052b3abd9f724c9fabe702a555acab82a
Chris Bird <drkvogel@yahoo.co.uk>
2018-09-21 16:08:41 +0100

    more

---
+++ blackjack.py
@@ -0,0 +1,22 @@
+"""
+Blackjack
+
+Play a game of Blackjack from the command line, one player against the dealer
+"""
+
+import curses
+
+def main():
+    pass
+
+def stuff():
+    print("Stuff")
+    name = input("What's your name?: ")
+    print("Hello", name)
+    # while 1:
+    #     key 
+
+
+if __name__ == "__main__":
+    print ("Hello World")
+    stuff()
